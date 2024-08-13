d47e8e3754df3b853ab77dfbae4c49f8648e3176
Vibha <vibha.gopal@gmail.com>
2024-08-13 14:38:00 +0530

    Get Spares and pending to form

--- custom/Extension/modules/relationships/vardefs/projecttask_aos_products_1_ProjectTask.php
+++ custom/Extension/modules/relationships/vardefs/projecttask_aos_products_1_ProjectTask.php
@@ -10,3 +10,24 @@ $dictionary["ProjectTask"]["fields"]["projecttask_aos_products_1"] = array (
   'side' => 'right',
   'vname' => 'LBL_PROJECTTASK_AOS_PRODUCTS_1_FROM_AOS_PRODUCTS_TITLE',
 );
+$dictionary["ProjectTask"]["fields"]["projecttask_aos_products_1_name"] = array (
+    'name' => 'projecttask_aos_products_1_name',
+    'type' => 'relate',
+    'source' => 'non-db',
+    'vname' => 'LBL_PROJECTTASK_AOS_PRODUCTS_1_FROM_AOS_PRODUCTS_TITLE',
+    'save' => true,
+    'id_name' => 'projecttask_aos_products_1projecttask_ida',
+    'link' => 'projecttask_aos_products_1',
+    'table' => 'AOS_Products',
+    'module' => 'AOS_Products',
+    'rname' => 'name',
+);
+$dictionary["ProjectTask"]["fields"]["projecttask_aos_products_1projecttask_ida"] = array (
+    'name' => 'projecttask_aos_products_1projecttask_ida',
+    'type' => 'link',
+    'relationship' => 'projecttask_aos_products_1',
+    'source' => 'non-db',
+    'reportable' => false,
+    'side' => 'right',
+    'vname' => 'LBL_PROJECTTASK_AOS_PRODUCTS_1_FROM_AOS_PRODUCTS_TITLE',
+);
